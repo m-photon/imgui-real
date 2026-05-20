14fe47bbb05eb07b63fb6847a39dee06da6134fe
m-photon <averagejellym.m@gmail.com>
2026-05-20 16:33:30 -0500

    Create v111.loader.js

---
+++ games/build/v111.loader.js
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<!-- saved from url=(0044)https://map-l.github.io/play/ragdollarchers/ -->
+<html lang="en-us"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<script src="./Ragdoll Archers_files/gameloader-helper.js.download"></script>
+
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+<title>Ragdoll Archers</title>
+<link rel="stylesheet" href="./Ragdoll Archers_files/style.css">
+</head><body>
+<div id="unity-container" class="unity-desktop">
+<canvas id="unity-canvas" width="1912" height="956" style="cursor: default;"></canvas>
+</div>
+<div id="loading-cover" style="display: none;">
+<div id="unity-loading-bar">
+<div id="unity-logo"><img src="./Ragdoll Archers_files/logo.jpeg"></div>
+<div class="progress-spinner-container">
+<div class="spinner-left"><img src="./Ragdoll Archers_files/spinner.png"></div>
+<div id="unity-progress-bar-empty">
+<div id="unity-progress-bar-full" style="width: 100%;"></div>
+</div>
+<div class="spinner-right"><img src="./Ragdoll Archers_files/spinner.png"></div>
+</div>
+</div>
+</div>
+<script>
+    const buildUrl = "Build";
+    const loaderUrl = buildUrl + "/v111.loader.js";
+    const config = {
+      dataUrl: buildUrl + "/6697b6deba8b617c67b69c55dcd07cb1.data.unityweb",
+      frameworkUrl: buildUrl + "/0be3c3ee7115b26e1ae28e643afdf1f2.js.unityweb",
+      codeUrl: buildUrl + "/b3397c29f6878557c1c614cbf9e196ea.wasm.unityweb",
+      streamingAssetsUrl: "StreamingAssets",
+      companyName: "Ericetto",
+      productName: "Ragdoll Archers",
+      productVersion: "0.1",
+    };
+
+    const container = document.querySelector("#unity-container");
+    const canvas = document.querySelector("#unity-canvas");
+    const loadingCover = document.querySelector("#loading-cover");
+    const progressBarEmpty = document.querySelector("#unity-progress-bar-empty");
+    const progressBarFull = document.querySelector("#unity-progress-bar-full");
+
+    if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+      container.className = "unity-mobile";
+    }
+    
+    loadingCover.style.display = "";
+
+    const unityLoader = document.createElement("script");
+    unityLoader.src = loaderUrl;
+    unityLoader.onload = () => {
+      createUnityInstance(canvas, config, (progress) => {
+        progressBarEmpty.style.display = "";
+        progressBarFull.style.width = `${100 * progress}%`;
+      }).then((unityInstance) => {
+        window.unityInstance = unityInstance;
+        loadingCover.style.display = "none";
+      }).catch((message) => {
+        alert(message);
+      });
+    };
+    document.body.appendChild(unityLoader);
+
+    document.addEventListener("pointerdown", () => {
+      container.focus();
+      window.focus();
+      canvas.focus();
+    });
+
+  </script><script src="./Ragdoll Archers_files/v111.loader.js.download"></script>
+<script src="blob:https://map-l.github.io/19f6cdb3-48ab-41bf-88a7-48d472d2aac3"></script></body></html>
